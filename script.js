19785723b914c495562ba8ef194410eefe9031e2
Ishan Kashyap <78429831+ishankrs@users.noreply.github.com>
2024-01-30 22:14:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var map = L.map('map').setView([20.5937, 78.9629], 5);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
+var map = L.map('map').setView([20.5937, 78.9629], 10);
 
-// Add a marker for testing
-L.marker([20.5937, 78.9629]).addTo(map);
+
 
 // Log to console for debugging
 console.log("Map and tile layer added successfully");
