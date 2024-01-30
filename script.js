db9a2f13726394df67473b80504bcb010757a2e7
Ishan Kashyap <78429831+ishankrs@users.noreply.github.com>
2024-01-30 22:12:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-var map = L.map('map', {
-    center: [20.5937, 78.9629],
-    zoom: 5
-});
+var map = L.map('map').setView([20.5937, 78.9629], 5);
 
-// Rest of your JavaScript code...
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// Add a marker for testing
+L.marker([20.5937, 78.9629]).addTo(map);
+
+// Log to console for debugging
+console.log("Map and tile layer added successfully");
